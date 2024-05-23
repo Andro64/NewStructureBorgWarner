f50cc5340b122a24c7e014d9438c9fe8ed6e96ef
GitHubAndro64 <bas1s@hotmail.com>
2024-05-23 18:12:04 -0500

    Se elimina el debug de los encoders

--- BORGWARNER_SERVOPRESS/BORGWARNER_SERVOPRESS.DataAccessLayer/CommunicationErgoArm.cs
+++ BORGWARNER_SERVOPRESS/BORGWARNER_SERVOPRESS.DataAccessLayer/CommunicationErgoArm.cs
@@ -52,9 +52,9 @@ namespace BORGWARNER_SERVOPRESS.DataAccessLayer
                 ushort[] dataErgoArm = modbusIPMaster.ReadInputRegisters(startingAddressOfInputRegisters, numberRegisterToRead);
                 sessionApp.positionErgoArm.encoder1 = -(Convert.ToDouble(dataErgoArm[0]) * 0.0036) + 110.94;
                 sessionApp.positionErgoArm.encoder2 = (Convert.ToDouble(dataErgoArm[1]) * 0.0132) - 106.57;
-                Debug.WriteLine($"{DateTime.Now} - encoder1: " + sessionApp.positionErgoArm.encoder1 + " encoder2: " + sessionApp.positionErgoArm.encoder2 + " Leyendo la posicion del ErgoArm");
+                //Debug.WriteLine($"{DateTime.Now} - encoder1: " + sessionApp.positionErgoArm.encoder1 + " encoder2: " + sessionApp.positionErgoArm.encoder2 + " Leyendo la posicion del ErgoArm");
                 validatePosition(screw);
-                Debug.WriteLine($"{DateTime.Now} - encoder1: " + sessionApp.positionErgoArm.encoder1 + " encoder2: " + sessionApp.positionErgoArm.encoder2 + " Validando la posicion del ErgoArm");
+                //Debug.WriteLine($"{DateTime.Now} - encoder1: " + sessionApp.positionErgoArm.encoder1 + " encoder2: " + sessionApp.positionErgoArm.encoder2 + " Validando la posicion del ErgoArm");
                 //Debug.WriteLine($"{DateTime.Now} - "  + "Validando la posicion del ErgoArm");
                 Thread.Sleep(50);
             }
